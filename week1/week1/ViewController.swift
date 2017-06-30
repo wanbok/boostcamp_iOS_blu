09fa5a99077cee5d8c0197f316bba0305c66b7ca
BLU <pom0894@naver.com>
2017-06-30 13:56:08 +0900

    init repo and add week1 project

---
+++ week1/week1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by BLU on 2017. 6. 30..
+//  Copyright © 2017년 futr_blu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
